f89aca05fa03fa7f46a16252bbd6100c295266cb
Volodymyr Popovichenko <vovvva33@gmail.com>
2020-04-12 15:53:53 +0300

    lint

--- src/common/jwtMiddelware.js
+++ src/common/jwtMiddelware.js
@@ -1,26 +1,25 @@
-const jwt = require("jsonwebtoken");
-const config = require("../config");
+const jwt = require('jsonwebtoken');
+const config = require('../config');
 
 const jwtMiddelware = async (req, res, next) => {
   try {
     const { headers } = req;
-    const userToken = headers["X-auth-token"];
-    const apiKey = headers["X-api-key"];
+    const userToken = headers['X-auth-token'];
+    const apiKey = headers['X-api-key'];
     const sessionToken = req.session.token;
 
     if (apiKey === config.api_key) return next();
 
-    if (!sessionToken) return res.status(404).send("session token was missing");
+    if (!sessionToken) return res.status(404).send('session token was missing');
     const { data } = jwt.verify(sessionToken, config.secret_key);
 
-    if (!userToken) return res.status(404).send("token a missing");
+    if (!userToken) return res.status(404).send('token a missing');
     const validToken = await jwt.verify(userToken, config.secret_key);
 
-    if (!validToken) return res.status(404).send("token not valid");
+    if (!validToken) return res.status(404).send('token not valid');
 
-    if (validToken.data.id !== data.id)
-      return res.status(404).send("ha ha your token invalid");
-    next();
+    if (validToken.data.id !== data.id) return res.status(404).send('ha ha your token invalid');
+    return next();
   } catch (err) {
     throw new Error(err);
   }
