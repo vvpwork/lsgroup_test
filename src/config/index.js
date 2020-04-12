759b5198a02f9dbc22a9580e8af6f6185e72dbf8
Volodymyr Popovichenko <vovvva33@gmail.com>
2020-04-12 16:47:57 +0300

    lint

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@ module.exports = {
   port: process.env.PORT,
   sentry: {
     dsn:
-      "https://9357006a9dfa42c7a3e55252edbb240e@o375029.ingest.sentry.io/5193885",
+      'https://9357006a9dfa42c7a3e55252edbb240e@o375029.ingest.sentry.io/5193885',
   },
   db: {
     password: process.env.DB_PASSWORD,
